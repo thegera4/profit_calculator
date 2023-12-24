749e9583d7fd98f4d1a3f4d6d4102124ec6990ec
thegera4 <84020433+thegera4@users.noreply.github.com>
2023-12-23 19:36:55 -0600

    update: input validation and save results to a file

--- profit_calculator.go
+++ profit_calculator.go
@@ -4,24 +4,47 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"errors"
 )
 
 func main() {
-	revenue := askForInput("Enter revenue: ")
-	expenses := askForInput("Enter expenses: ")
-	taxRate := askForInput("Enter tax rate: ")
+
+	revenue, revenueErr := askForInput("Enter revenue: ")
+
+	if revenueErr != nil {
+		fmt.Println("Please enter a valid revenue amount.")
+		return
+	}
+
+	expenses, expensesErr := askForInput("Enter expenses: ")
+
+	if expensesErr != nil {
+		fmt.Println("Please enter a valid expenses amount.")
+		return
+	}
+
+	taxRate, taxErr := askForInput("Enter tax rate: ")
+
+	if taxErr != nil {
+		fmt.Println("Please enter a valid tax rate.")
+		return
+	}
 
 	ebt, profit, ratio := calculations(revenue, expenses, taxRate)
-
+	storeResults(ebt, profit, ratio)
 	printResults(ebt, profit, ratio)
 	waitForEnter()
 }
 
-func askForInput(text string) float64 {
+func askForInput(text string) (float64, error) {
 	var input float64
 	fmt.Println(text)
 	fmt.Scanln(&input)
-	return input
+	if input <= 0 {
+		fmt.Println("Exiting program...")
+		return 0, errors.New("invalid input")
+	}
+	return input, nil
 }
 
 func calculations(revenue, expenses, taxRate float64) (float64, float64, float64) {
@@ -29,6 +52,11 @@ func calculations(revenue, expenses, taxRate float64) (float64, float64, float64
 	profit := ebt * (1 - taxRate/100)
 	ratio := ebt / profit
 	return ebt, profit, ratio
+}
+
+func storeResults(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("Your Earnings Before Tax (EBT): %.2f\nYour Profit: %.2f\nYour Ratio: %.2f\n", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
 }
 
 func printResults(ebt, profit, ratio float64) {
