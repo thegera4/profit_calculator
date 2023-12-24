35fafde96a6d92b43214aee0b0dbbf64fe2bdf11
thegera4 <84020433+thegera4@users.noreply.github.com>
2023-12-23 18:03:32 -0600

    update: functions approach

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/thegera4/example
-
-go 1.21.5
--- profit_calculator.go
+++ profit_calculator.go
@@ -7,35 +7,38 @@ import (
 )
 
 func main() {
-	var revenue, expenses, taxRate float64
+	revenue := askForInput("Enter revenue: ")
+	expenses := askForInput("Enter expenses: ")
+	taxRate := askForInput("Enter tax rate: ")
 
-	fmt.Println("Enter revenue: ")
-	fmt.Scanln(&revenue)
+	ebt, profit, ratio := calculations(revenue, expenses, taxRate)
 
-	fmt.Println("Enter expenses: ")
-	fmt.Scanln(&expenses)
-
-	fmt.Println("Enter tax rate: ")
-	fmt.Scanln(&taxRate)
-
-	ebt := revenue - expenses
-	profit := ebt * (1 - taxRate/100)
-	ratio := ebt / profit
-
-	fmt.Println("Your Earnings Before Tax (EBT): ")
-	fmt.Println(ebt)
-
-	fmt.Println("Your Profit: ")
-	fmt.Println(profit)
-
-	fmt.Println("Your Ratio: ")
-	fmt.Println(ratio)
-
-	fmt.Println("Press Enter to exit...")
+	printResults(ebt, profit, ratio)
 	waitForEnter()
 }
 
+func askForInput(text string) float64 {
+	var input float64
+	fmt.Println(text)
+	fmt.Scanln(&input)
+	return input
+}
+
+func calculations(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+	return ebt, profit, ratio
+}
+
+func printResults(ebt, profit, ratio float64) {
+	fmt.Printf("Your Earnings Before Tax (EBT): %.2f\n", ebt)
+	fmt.Printf("Your Profit: %.2f\n", profit)
+	fmt.Printf("Your Ratio: %.2f\n", ratio)
+}
+
 func waitForEnter() {
+	fmt.Println("Press enter to exit...")
 	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan() // Wait for input
+	scanner.Scan()
 }
